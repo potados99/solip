8636580398f13176152b129dd16a918ede4c5dfa
potados99 <potados99@gmail.com>
2025-11-13 17:11:37 +0900

    fix: non-workspaces 환경에서 findApiRootPath가 작동하도록, INIT_CWD 없으면 클래식하게 작동하도록 코드 복구

--- packages/solip/src/utils/path-utils.ts
+++ packages/solip/src/utils/path-utils.ts
@@ -1,3 +1,6 @@
+import path from "path";
+import { existsSync } from "fs";
+
 export function findApiRootPath() {
   // NOTE: for support npm / yarn workspaces
   const workspacePath = process.env["INIT_CWD"];
@@ -11,9 +14,22 @@ export function findApiRootPath() {
   // swc 출력을 es2015까지 낮춰서 성공하는 현상을 목격했는데,,, 이제는 재현이 안 됨...!
   // 그래서 swc 출력은 다시 esnext로 해둠.
 
+  const basePath = import.meta.filename;
+  let dir = path.dirname(basePath);
+  if (dir.includes("/.yarn/")) {
+    dir = dir.split("/.yarn/")[0];
+  }
+  do {
+    if (existsSync(path.join(dir, "/package.json"))) {
+      return dir.split(path.sep).join(path.sep);
+    }
+    dir = dir.split(path.sep).slice(0, -1).join(path.sep);
+  } while (dir.split(path.sep).length > 1);
+  
   throw new Error("프로젝트 루트 경로를 찾을 수 없습니다.");
 
   // import.meta.filename이 요따구로 나옴: /Users/potados/Projects/solip/.yarn/__virtual__/solip-virtual-053ce12b7d/1/packages/solip/dist/utils/path-utils.js
   // 그래서 찾아봤자 /Users/potados/Projects/solip 이런 식으로 나옴
   // 결국 이 경로에서는 실제 프로젝트 경로를 못 구함.
+  // 그치만 workspace 환경이 아니면 작동할 것임!
 }
